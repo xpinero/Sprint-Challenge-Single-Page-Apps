cb0d7ff51d7de1b2398667df773ce7f4dbaa5324
Xaver <xaverpinero@yahoo.com>
2020-03-22 19:53:32 -0400

    added imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage';
 
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <Route path='./components/WelcomePage' component={WelcomePage} />
     </main>
   );
 }
