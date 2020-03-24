50eab0d402b70a9e0d9cb70796c3d21983ddcc0b
Xaver <xaverpinero@yahoo.com>
2020-03-24 15:03:06 -0400

    tried some stuff

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function SearchForm(props) {
 
@@ -16,6 +16,7 @@ export default function SearchForm(props) {
       />
       <Link to="/"><div className="home-button">Home</div></Link>
     </section>
+    
     
   );
 }
